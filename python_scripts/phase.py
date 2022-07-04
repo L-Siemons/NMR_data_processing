78936843c567cac0d8df55138cf663631d69bfb4
Olof Stenström <olof@majorana.chimie.ens.fr>
2022-07-04 17:13:11 +0200

    2022/07/04 contours!

--- python_scripts/phase.py
+++ python_scripts/phase.py
@@ -8,9 +8,13 @@ from matplotlib.widgets import Slider, Button
 
 # plot parameters these should be changed by the user if needed
 spectrum = 'test.ft2'
+# read in the data from a NMRPipe file
+dic, data = ng.pipe.read(spectrum)
+
 cmap = matplotlib.cm.Blues_r    # contour map (colors to use for contours)
 
-contour_start = 100000000000           # contour level start value
+
+contour_start = np.std(data)*2          # contour level start value
 contour_num = 50                # number of contour levels
 contour_factor = 1.25          # scaling factor between contour levels
 contour_line_width = 0.4
@@ -25,11 +29,16 @@ yp0_val = None
 yp1_val = None
 
 # calculate contour levels
-cl = contour_start * contour_factor ** np.arange(contour_num)
-# read in the data from a NMRPipe file
-dic, data = ng.pipe.read(spectrum)
-#data = hilbert(data)
 
+cl = contour_start * contour_factor ** np.arange(contour_num)
+
+
+
+# create the figure
+
+fig, ax= plt.subplots(1, 1)
+fig.set_figheight(8)
+fig.set_figwidth(13)
 
 def onclick(event):
 
@@ -52,7 +61,6 @@ def onclick(event):
 
 def update(val):
 
-
     xslice = data[dim_uc[0](f'{iy} ppm'), :]
     xslice = hilbert(xslice)
     di, xslice = ng.pipe_proc.ps(dic, xslice, p0=x_p0_slider.val, p1=x_p1_slider.val)
@@ -64,22 +72,15 @@ def update(val):
     plot_yslice.set_xdata( -yslice / y_intensity.val + ix)
     fig.canvas.draw_idle()
 
-
-# make ppm scales
-dim_uc = {}
-dim_ppm_scale = {}
-dim_ppm_limits = {}
-
-for i in range(len(data.shape)):
-
-    dim_uc[i] = ng.pipe.make_uc(dic, data, dim=i)
-    dim_ppm_scale[i] = dim_uc[i].ppm_scale()
-    dim_ppm_limits[i] = dim_uc[i].ppm_limits()
-# create the figure
-
-fig, ax= plt.subplots(1, 1)
-fig.set_figheight(8)
-fig.set_figwidth(13)
+def updatecont(val):
+    for coll in ax.collections:
+        ax.collections.remove(coll) 
+    #contour_axis.clear()
+    cl = np.std(data)*contour_lvl.val * contour_factor ** np.arange(contour_num)
+    ax.contour(data, cl, cmap=cmap,
+            extent=(dim_ppm_limits[1][0], dim_ppm_limits[1][1], dim_ppm_limits[0][0], dim_ppm_limits[0][1]), 
+            zorder=2, linewidths=contour_line_width)
+    fig.canvas.draw_idle()
 
 axp0 = plt.axes([0.12, 0.0, 0.2, 0.03])
 x_intensity = Slider(
@@ -135,11 +136,34 @@ y_p0_slider = Slider(
     valinit=0,
 )
 
+axp0 = plt.axes([0.03, 0.68, 0.2, 0.03])
+contour_lvl = Slider(
+    ax=axp0,
+    label='contour level',
+    valmin=0,
+    valmax=5,
+    valinit=2,
+)
+
+
+# make ppm scales
+dim_uc = {}
+dim_ppm_scale = {}
+dim_ppm_limits = {}
+
+for i in range(len(data.shape)):
+
+    dim_uc[i] = ng.pipe.make_uc(dic, data, dim=i)
+    dim_ppm_scale[i] = dim_uc[i].ppm_scale()
+    dim_ppm_limits[i] = dim_uc[i].ppm_limits()
+
+
 
 # plot the contours
 ax.contour(data, cl, cmap=cmap,
             extent=(dim_ppm_limits[1][0], dim_ppm_limits[1][1], dim_ppm_limits[0][0], dim_ppm_limits[0][1]), 
             zorder=2, linewidths=contour_line_width)
+contour_axis = plt.gca()
 
 # plot slices in each direction
 iy = np.mean(dim_ppm_limits[0])
@@ -158,6 +182,7 @@ x_p0_slider.on_changed(update)
 x_p1_slider.on_changed(update)
 y_p0_slider.on_changed(update)
 y_p1_slider.on_changed(update)
+contour_lvl.on_changed(updatecont)
 
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 plt.xlabel('1H (ppm)')
